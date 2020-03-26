2c6a279d2bb97850e89b956c993d25c7c1806b56
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-26 14:00:52 -0400

    Fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -27,9 +27,8 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      filename: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL,
+  
     migrations: {
       directory: "./data/migrations",
     },
