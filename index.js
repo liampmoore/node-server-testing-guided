c859312c5af9aa89a180864bb7e9276225c6d22e
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-26 13:42:50 -0400

    Switched to DB_ENV instead of NODE_ENV

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.DB_ENV || 'development';
 server.listen(port, () => console.log(`\n** server up on port ${port} in ${environment} environment **\n`));
